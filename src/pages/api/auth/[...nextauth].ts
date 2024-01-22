c076e4b83ab03585497320ee489960e8de0817c0
Gabriel Santana <santanag311@gmail.com>
2024-01-22 11:38:18 -0300

    first commit

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,17 @@
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions = {
+    providers:[
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            httpOptions: {
+                timeout: 40000,
+              },
+        }),
+    ],
+
+}
+
+export default NextAuth(authOptions)
